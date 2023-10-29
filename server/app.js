713f7e912dcafcd2da1aec322f98b15019f6ecca
juliochandra <juliochandra@gmail.com>
2023-10-29 15:13:21 +0700

    update view

--- server/app.js
+++ server/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const path = require("path");
 const destinationsRouter = require("./routes/destinationRoutes");
-const ejsMate = require("ejs-mate");
 
 const app = express();
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
+const ejsMate = require("ejs-mate");
+
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(morgan("dev"));
 
 // server listening
 const port = 3003;
@@ -24,6 +29,7 @@ const getHome = async (req, res) => {
     });
   }
 };
+
 //  route
 app.get("/", getHome);
 app.use("/api/v1/destinations", destinationsRouter);
